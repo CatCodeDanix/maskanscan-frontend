6a8cb0d912fc3d18d7b9ac353d227417830b8497
Danial Abdoli <CatCodeDanix@users.noreply.github.com>
2026-05-16 01:51:57 +0330

    Template project

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="fa"
+      className={`
+        ${geistSans.variable}
+        ${geistMono.variable}
+        h-full antialiased
+      `}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
